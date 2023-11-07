ce12801a96739de032f36660dee05a98c615cdbf
Tomer <tomerrim@gmail.com>
2023-11-07 17:54:30 +0200

    added JWT Strategy

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -17,7 +17,7 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const salt = await bcrypt.getSalt();
+    const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const user = this.create({ username, password: hashedPassword });
